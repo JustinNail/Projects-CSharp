99ea286b5d1a97e03d360b91ea744579cf9f3702
Justin Nail <JustinNail@gmail.com>
2013-03-02 05:52:09 -0500

    Initial Daemonhost Generator Commit

---
+++ DaemonhostGenerator/DaemonhostGenerator/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace DaemonhostGenerator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.Fel_Box = new System.Windows.Forms.TextBox();
+			this.Wp_Box = new System.Windows.Forms.TextBox();
+			this.Per_Box = new System.Windows.Forms.TextBox();
+			this.Int_Box = new System.Windows.Forms.TextBox();
+			this.Ag_Box = new System.Windows.Forms.TextBox();
+			this.T_Box = new System.Windows.Forms.TextBox();
+			this.S_Box = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.BS_Box = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.WS_Box = new System.Windows.Forms.TextBox();
+			this.Binding_comboBox = new System.Windows.Forms.ComboBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.Wounds_Box = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.Traits_Box = new System.Windows.Forms.TextBox();
+			this.Talents_Box = new System.Windows.Forms.TextBox();
+			this.Skills_Box = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.Wounds_Box);
+			this.groupBox1.Controls.Add(this.Fel_Box);
+			this.groupBox1.Controls.Add(this.Wp_Box);
+			this.groupBox1.Controls.Add(this.Per_Box);
+			this.groupBox1.Controls.Add(this.Int_Box);
+			this.groupBox1.Controls.Add(this.Ag_Box);
+			this.groupBox1.Controls.Add(this.T_Box);
+			this.groupBox1.Controls.Add(this.S_Box);
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.BS_Box);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.WS_Box);
+			this.groupBox1.Location = new System.Drawing.Point(12, 57);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(131, 267);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Characteristics";
+			// 
+			// Fel_Box
+			// 
+			this.Fel_Box.Location = new System.Drawing.Point(81, 216);
+			this.Fel_Box.Name = "Fel_Box";
+			this.Fel_Box.ReadOnly = true;
+			this.Fel_Box.Size = new System.Drawing.Size(44, 20);
+			this.Fel_Box.TabIndex = 17;
+			// 
+			// Wp_Box
+			// 
+			this.Wp_Box.Location = new System.Drawing.Point(81, 191);
+			this.Wp_Box.Name = "Wp_Box";
+			this.Wp_Box.ReadOnly = true;
+			this.Wp_Box.Size = new System.Drawing.Size(44, 20);
+			this.Wp_Box.TabIndex = 16;
+			// 
+			// Per_Box
+			// 
+			this.Per_Box.Location = new System.Drawing.Point(81, 166);
+			this.Per_Box.Name = "Per_Box";
+			this.Per_Box.ReadOnly = true;
+			this.Per_Box.Size = new System.Drawing.Size(44, 20);
+			this.Per_Box.TabIndex = 15;
+			// 
+			// Int_Box
+			// 
+			this.Int_Box.Location = new System.Drawing.Point(81, 141);
+			this.Int_Box.Name = "Int_Box";
+			this.Int_Box.ReadOnly = true;
+			this.Int_Box.Size = new System.Drawing.Size(44, 20);
+			this.Int_Box.TabIndex = 14;
+			// 
+			// Ag_Box
+			// 
+			this.Ag_Box.Location = new System.Drawing.Point(81, 116);
+			this.Ag_Box.Name = "Ag_Box";
+			this.Ag_Box.ReadOnly = true;
+			this.Ag_Box.Size = new System.Drawing.Size(44, 20);
+			this.Ag_Box.TabIndex = 13;
+			// 
+			// T_Box
+			// 
+			this.T_Box.Location = new System.Drawing.Point(81, 91);
+			this.T_Box.Name = "T_Box";
+			this.T_Box.ReadOnly = true;
+			this.T_Box.Size = new System.Drawing.Size(44, 20);
+			this.T_Box.TabIndex = 12;
+			// 
+			// S_Box
+			// 
+			this.S_Box.Location = new System.Drawing.Point(81, 66);
+			this.S_Box.Name = "S_Box";
+			this.S_Box.ReadOnly = true;
+			this.S_Box.Size = new System.Drawing.Size(44, 20);
+			this.S_Box.TabIndex = 11;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(7, 169);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(23, 13);
+			this.label9.TabIndex = 10;
+			this.label9.Text = "Per";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(7, 194);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(24, 13);
+			this.label8.TabIndex = 9;
+			this.label8.Text = "Wp";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(7, 219);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(21, 13);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "Fel";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(7, 119);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(20, 13);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "Ag";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(7, 144);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(19, 13);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Int";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(7, 69);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(14, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "S";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(7, 94);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(14, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "T";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(7, 44);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(21, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "BS";
+			// 
+			// BS_Box
+			// 
+			this.BS_Box.Location = new System.Drawing.Point(81, 41);
+			this.BS_Box.Name = "BS_Box";
+			this.BS_Box.ReadOnly = true;
+			this.BS_Box.Size = new System.Drawing.Size(44, 20);
+			this.BS_Box.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(25, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "WS";
+			// 
+			// WS_Box
+			// 
+			this.WS_Box.Location = new System.Drawing.Point(81, 16);
+			this.WS_Box.Name = "WS_Box";
+			this.WS_Box.ReadOnly = true;
+			this.WS_Box.Size = new System.Drawing.Size(44, 20);
+			this.WS_Box.TabIndex = 0;
+			// 
+			// Binding_comboBox
+			// 
+			this.Binding_comboBox.FormattingEnabled = true;
+			this.Binding_comboBox.Location = new System.Drawing.Point(4, 18);
+			this.Binding_comboBox.Name = "Binding_comboBox";
+			this.Binding_comboBox.Size = new System.Drawing.Size(121, 21);
+			this.Binding_comboBox.TabIndex = 1;
+			this.Binding_comboBox.SelectedIndexChanged += new System.EventHandler(this.Binding_comboBox_SelectedIndexChanged);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.Binding_comboBox);
+			this.groupBox2.Location = new System.Drawing.Point(12, 6);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(131, 45);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Binding";
+			// 
+			// Wounds_Box
+			// 
+			this.Wounds_Box.Location = new System.Drawing.Point(81, 241);
+			this.Wounds_Box.Name = "Wounds_Box";
+			this.Wounds_Box.ReadOnly = true;
+			this.Wounds_Box.Size = new System.Drawing.Size(44, 20);
+			this.Wounds_Box.TabIndex = 18;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(7, 244);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(47, 13);
+			this.label10.TabIndex = 19;
+			this.label10.Text = "Wounds";
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Location = new System.Drawing.Point(150, 13);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(200, 155);
+			this.tabControl1.TabIndex = 3;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+			this.tabPage1.Controls.Add(this.Traits_Box);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(192, 129);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Traits";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+			this.tabPage2.Controls.Add(this.Talents_Box);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 129);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Talents";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.BackColor = System.Drawing.SystemColors.Control;
+			this.tabPage3.Controls.Add(this.Skills_Box);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(192, 129);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Skills";
+			// 
+			// Traits_Box
+			// 
+			this.Traits_Box.Location = new System.Drawing.Point(6, 6);
+			this.Traits_Box.Multiline = true;
+			this.Traits_Box.Name = "Traits_Box";
+			this.Traits_Box.ReadOnly = true;
+			this.Traits_Box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.Traits_Box.Size = new System.Drawing.Size(180, 117);
+			this.Traits_Box.TabIndex = 20;
+			// 
+			// Talents_Box
+			// 
+			this.Talents_Box.Location = new System.Drawing.Point(6, 6);
+			this.Talents_Box.Multiline = true;
+			this.Talents_Box.Name = "Talents_Box";
+			this.Talents_Box.ReadOnly = true;
+			this.Talents_Box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.Talents_Box.Size = new System.Drawing.Size(180, 117);
+			this.Talents_Box.TabIndex = 21;
+			// 
+			// Skills_Box
+			// 
+			this.Skills_Box.Location = new System.Drawing.Point(6, 6);
+			this.Skills_Box.Multiline = true;
+			this.Skills_Box.Name = "Skills_Box";
+			this.Skills_Box.ReadOnly = true;
+			this.Skills_Box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.Skills_Box.Size = new System.Drawing.Size(180, 117);
+			this.Skills_Box.TabIndex = 21;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 356);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox Fel_Box;
+		private System.Windows.Forms.TextBox Wp_Box;
+		private System.Windows.Forms.TextBox Per_Box;
+		private System.Windows.Forms.TextBox Int_Box;
+		private System.Windows.Forms.TextBox Ag_Box;
+		private System.Windows.Forms.TextBox T_Box;
+		private System.Windows.Forms.TextBox S_Box;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox BS_Box;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox WS_Box;
+		private System.Windows.Forms.ComboBox Binding_comboBox;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox Wounds_Box;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TextBox Traits_Box;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TextBox Talents_Box;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TextBox Skills_Box;
+	}
+}
+
---
+++ DaemonhostGenerator/DaemonhostGenerator/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DaemonhostGenerator
+{
+	public partial class Form1 : Form
+	{
+		Random Dice = new Random();
+		public Form1()
+		{
+			InitializeComponent();
+			Binding_comboBox.Items.Add("Unbound");
+			Binding_comboBox.Items.Add("Once-bound");
+			Binding_comboBox.Items.Add("Twice-bound");
+			Binding_comboBox.Items.Add("Thrice-bound");
+		}
+
+		private void Binding_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			switch (Binding_comboBox.SelectedItem.ToString())
+			{
+				case "Unbound":
+					UnboundRoll();
+					break;
+				case "Once-bound":
+					OnceBoundRoll();
+					break;
+				case "Twice-bound":
+					TwiceBoundRoll();
+					break;
+				case "Thrice-bound":
+					ThriceBoundRoll();
+					break;
+			}
+		}
+
+		private void UnboundRoll()
+		{
+			UnboundStats();
+			int numrolls = Dice.Next(1, 6) + 1;
+			List<int> rolls = new List<int>();
+			while (true)
+			{
+				int roll = Dice.Next(1, 101);
+				if (roll >= 97)
+				{
+					rolls.Clear();
+					rolls.Add(roll);
+					break;
+				}
+				else
+				{
+					if (!rolls.Contains(roll))
+					{
+						rolls.Add(roll);
+					}
+					if (rolls.Count >= numrolls)
+					{
+						break;
+					}
+				}
+				
+			}
+			foreach (int roll in rolls)
+			{
+				UnholyChanges(roll);
+			}
+		}
+
+		private void UnholyChanges(int roll)
+		{
+			if(roll >= 1 && roll <= 4)
+			{Vestigial Horns: Two small horns project from the Daemonhost’s forehead.
+
+			}
+			else if(roll >= 5 && roll <= 8)
+			{Glowing Eyes: The Daemonhost’s eyes glow with a malign light.
+
+			}
+			else if(roll >= 9 && roll <= 12)
+			{Boils: The flesh of the Daemonhost is covered with weeping boils and sores.
+
+			}
+			else if(roll >= 13 && roll <= 16)
+			{Cat’s Eyes: The Daemonhost’s eyes are yellow and the pupils are black slits.
+
+			}
+			else if(roll >= 17 && roll <= 20)
+			{Horns: A set of horns, like those of a ram or goat, spring from the Daemonhost’s head. It gains the Natural Weapon (Horns) trait.
+
+			}
+			else if(roll >= 21 && roll <= 24)
+			{Claws or Blades: The Daemonhost’s fingers are elongated and sharpened into razor claws, or fused into blades as sharp as sin. It gains the 
+   Natural Weapon (Claws) trait with the Warp Weapon quality.
+
+			}
+			else if(roll >= 25 && roll <= 28)
+			{Broken Form: The Daemonhost’s body is permanently contorted, its body folded in unnatural ways.
+
+			}
+			else if(roll >= 29 && roll <= 32)
+			{Snake Tongue: A long forked tongue flicks from between the Daemonhost’s teeth.
+
+			}
+ 			else if(roll >= 33 && roll <= 36)
+			{Fluid Form: The Daemonhost’s flesh flows continually: limbs, screaming faces and other terrible things pushing out from its body before 
+   being reabsorbed.
+
+			}
+			else if(roll >= 37 && roll <= 40)
+			{Wings: Great wings of feathers or stretched skin have sprung from the Daemonhost’s back. It gains the Flyer trait at a rate equal to twice its 
+   Agility Bonus.
+
+			}
+			else if(roll >= 41 && roll <= 44)
+			{Bleeding Mouth and Eyes: The Daemonhost’s eyes continually weep blood, while viscous gore seeps from between its lips.
+
+			}
+			else if(roll >= 45 && roll <= 48)
+			{Covered in Eyes: The Daemonhost’s flesh is covered in eyes. The Daemonhost gains a +20 bonus to Awareness Tests involving vision.
+
+			}
+			else if(roll >= 49 && roll <= 52)
+			{Quills: The Daemonhost’s flesh has sprouted long avian quills. It gains the Natural Armour 2 trait.
+
+			}
+			else if(roll >= 53 && roll <= 56)
+			{Un-fleshed: The Daemonhost has no skin, its glistening muscles and sinews are exposed.
+
+			}
+			else if(roll >= 57 && roll <= 60)
+			{Inner Fire: An unearthly fire burns within the Daemonhost, glowing through its flesh, veins and skin. Its natural attacks inflict Energy 
+   Damage.
+
+			}
+			else if(roll >= 61 && roll <= 64)
+			{Bloated Form: The host body is grossly bloated.
+
+			}
+			else if(roll >= 65 && roll <= 68)
+			{Snake Nest: Snakes coil around the Daemonhost, flowing from its mouth and rents in its flesh. Whenever it deals Damage, the target must 
+   succeed on a Hard (-20) Toughness Test or be affected as if by a hallucinogen grenade for 1d5 Rounds.
+
+			}
+			else if(roll >= 69 && roll <= 72)
+			{Insect Hive: The Daemonhost’s body is a hive for a mass of insects that crawl across it. It gains 1d10 Wounds.
+
+			}
+			else if(roll >= 73 && roll <= 76)
+			{Corpse-Host: The host’s body has expired during the ritual and is visibly rotting—yet it still lives! It increases its Toughness by 1d10.
+
+			}
+			else if(roll >= 77 && roll <= 80)
+			{Elongated limbs: The Daemonhost’s limbs are distorted and elongated. The Daemonhost can attack creatures up to three metres away in 
+   close combat.
+
+			}
+			else if(roll >= 81 && roll <= 84)
+			{Scales: The Daemonhost’s body is covered in a fine layer of snake-like scales. It gains the Natural Armour 3 trait.
+
+			}
+			else if(roll >= 85 && roll <= 88)
+			{Animalistic: The Daemonhost’s body has bestial features, such as the head of a goat, bull or avian, backwards-jointed limbs or fur.
+
+			}
+			else if(roll >= 89 && roll <= 92)
+			{Featureless Face: Though it has no effect on its senses, the Daemonhost’s head is smooth, featureless flesh.
+
+			}
+			else if(roll >= 93 && roll <= 96)
+			{Charred Form: The host body appears horrifically burnt. The Daemonhost is immune to all forms of fire and heat damage, even psychic 
+   fire (but not holy flame).
+
+			}
+			else
+			{97–00  Seeming Normality: If you roll this result, do not roll any further on this table. Also, any results already rolled are removed. The host body 
+   seems perfectly normal, apart from the instruments of its binding. It adds 1d10 to its Fellowship.
+			}
+				
+		}
+
+		private void UnboundStats()
+		{
+			WS_Box.Text = (20 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			BS_Box.Text = (20 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			S_Box.Text = (45 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			T_Box.Text = (30 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Ag_Box.Text = (10 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Int_Box.Text = (70 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Per_Box.Text = (50 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Wp_Box.Text = (80 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Fel_Box.Text = (5 + Dice.Next(1,11) + Dice.Next(1,11)).ToString();
+			Wounds_Box.Text = "30";
+			Talents_Box.AppendText("Psy Rating 8");
+		}
+
+		private void OnceBoundRoll()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void TwiceBoundRoll()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ThriceBoundRoll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
