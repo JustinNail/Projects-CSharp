353692aff1d74c38b136422441b80c2e4f388a1d
Justin Nail <JustinNail@gmail.com>
2013-03-17 20:01:24 -0400

    adding ranks and advances

---
+++ DarkHeresyCharacterManager/DarkHeresyCharacterManager/Advance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DarkHeresyCharacter
+{
+	[Serializable()]
+	public class Advance
+	{
+		string Type;
+		int Cost;
+		string Name;
+
+		public Advance()
+		{
+			Type = "";
+			Cost = 0;
+			Name = "";
+		}
+		public Advance(string type, int cost, string name)
+		{
+			Type = type;
+			Cost = cost;
+			Name = name;
+		}
+	}
+}
---
+++ DarkHeresyCharacterManager/DarkHeresyCharacterManager/Rank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DarkHeresyCharacter
+{
+	[Serializable()]
+	public class Rank
+	{
+		public string Name { get; set; }
+		public int Num { get; set; }
+		public List<Advance> Advances;
+
+		public Rank()
+		{
+			Name = "";
+			Num = 0;
+			Advances = new List<Advance>();
+		}
+		public Rank(string name, int num, List<Advance> advances)
+		{
+			Name = name;
+			Num = num;
+			Advances = advances;
+		}
+	}
+}
