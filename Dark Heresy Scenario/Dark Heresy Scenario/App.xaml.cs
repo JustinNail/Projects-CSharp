2128f33527c75b22e459d2c1cb9379dd2caa97ca
Justin Nail <JustinNail@gmail.com>
2013-03-17 20:03:33 -0400

    Initial scenario commit

---
+++ Dark Heresy Scenario/Dark Heresy Scenario/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Dark_Heresy_Scenario
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
